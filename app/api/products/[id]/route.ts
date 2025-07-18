5287828890f7ca52f2369fbe780d6ab49b9c4c77
Agung Hutri <agung@qtasmin.com>
2025-07-18 15:25:39 +0700

    tes

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -3,18 +3,29 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function PUT(req: NextRequest, { params }: any) {
-  const { id } = params;
+export async function PUT(
+  req: NextRequest,
+  context: any
+) {
+  const { id } = await context.params;
   const body = await req.json();
+
   const updated = await prisma.product.update({
     where: { id },
-    data: { name: body.name, price: parseFloat(body.price) },
+    data: {
+      name: body.name,
+      price: parseFloat(body.price),
+    },
   });
+
   return NextResponse.json(updated);
 }
 
-export async function DELETE(_: NextRequest, { params }: any) {
-  const { id } = params;
+export async function DELETE(
+  req: NextRequest,
+  context: any
+) {
+  const { id } = await context.params;
   await prisma.product.delete({ where: { id } });
   return NextResponse.json({ success: true });
 }
